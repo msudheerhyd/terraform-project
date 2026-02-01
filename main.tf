ccec12701289306e8d5a9fafaf3131dd50d2bf6c
Pravin_Mishra <hello@pravinmishra.in>
2026-02-01 12:13:41 -0600

    Add Terraform project (excluding binaries and state)

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+    name = "example-resources"
+    location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+    name = "storage000121accountname"
+    resource_group_name = azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
+    account_tier = "Standard"
+    account_replication_type = "GRS"
+
+    tags = {
+        environment = "staging"
+    }
+}
